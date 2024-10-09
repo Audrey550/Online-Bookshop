7c6d90b0fe482f405ca1b8514640a06820c4f0d7
Audrey <r0982547@student.thomasmore.be>
2024-10-09 09:58:47 +0200

    feat: add homepage

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Audrey's Bookstore</title>
+</head>
+<body>
+    <h1>Online Bookshop</h1>
+</body>
+</html>
