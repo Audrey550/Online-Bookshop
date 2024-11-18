f5a20b4bff89f72bc5d364e1d03cbff67c3ae0a8
Audrey <r0982547@student.thomasmore.be>
2024-11-18 15:41:41 +0100

    Feat: added the admin.php link in the nav__logout div instead of a seperate div

--- nav.inc.php
+++ nav.inc.php
@@ -8,21 +8,18 @@
         <input type="text" name="search" placeholder="Search...">
     </form>
 
-    <!--INfo specifiek voor admins-->
-    <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true): ?>
-        <?php if($_SESSION['usertype'] == 1): ?>
-            <a href="admin.php">Admin pagina</a>
-        <?php endif ?>
-    <?php endif ?>
-
-    <!--Info specifiek voor gebruikers-->
     <div class="nav__logout">
-        <a href="logout.php" class="logout">Hi <?php echo htmlspecialchars($_SESSION['username']); ?>, logout?</a><!--username veranderen--> 
-        <a href="#"><img src="img/profileIcon.png"></a>
+            <!--INfo specifiek voor admins-->
+        <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true): ?>
+            <?php if($_SESSION['usertype'] == 1): ?>
+                <a href="admin.php">Admin pagina</a>
+            <?php endif ?>
+                <!--Info specifiek voor gebruikers-->
+                <a href="logout.php" class="logout">Hi <?php echo htmlspecialchars($_SESSION['username']); ?>, logout?</a><!--username veranderen--> 
+            <?php endif ?>
+                <a href="#"><img src="img/profileIcon.png"></a>
+                <a href="#"> <img src="img/shoppingIcon.png"></a>
     </div>
-
-    <div class="nav__shoppingCart">
-        <a href="#"> <img src="img/shoppingIcon.png"></a>
-    </div>
+    <!--<a href="#"> <img src="img/shoppingIcon.png"></a>-->    
 </nav>
 
