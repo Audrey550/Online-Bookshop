717892aa8180a5c41720e252bea8fe5e64eefe73
Audrey <r0982547@student.thomasmore.be>
2024-11-19 20:20:38 +0100

    feat: added the admin.php link onto the profilepicture. These pages change according to which usertype is logged in

--- nav.inc.php
+++ nav.inc.php
@@ -9,17 +9,23 @@
     </form>
 
     <div class="nav__logout">
-            <!--INfo specifiek voor admins-->
+            <!--Controleert of de gebruiker is ingelogd-->
         <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true): ?>
-            <?php if($_SESSION['usertype'] == 1): ?>
-                <a href="admin.php">Admin pagina</a>
-            <?php endif ?>
-                <!--Info specifiek voor gebruikers-->
-                <a href="logout.php" class="logout">Hi <?php echo htmlspecialchars($_SESSION['username']); ?>, logout?</a><!--username veranderen--> 
-            <?php endif ?>
-                <a href="#"><img src="img/profileIcon.png"></a>
-                <a href="#"> <img src="img/shoppingIcon.png"></a>
+            <!--Logout knop-->
+            <a href="logout.php" class="logout">Hi <?php echo htmlspecialchars($_SESSION['username']); ?>, logout?</a>
+
+            <!--Profiellink afhangend van wat soort gebruiker het is-->
+            <a href="<?php echo ($_SESSION['usertype'] == 1)? 'admin.php' : 'clientProfile.php'; ?>"><img src="img/profileIcon.png"></a>
+            <?php else: ?>
+
+            <!--Als de gebruiker niet is ingelogd, toon alleen de profiel icon zonder link-->
+            <a href="login.php"><img src="img/profileIcon.png"></a>
+        <?php endif; ?>
+
+        <!--Als de gebruiker een admin, word het winkelwagen icoon niet getoont-->
+        <?php if (isset($_SESSION['loggedin']) && $_SESSION['usertype'] === 0): ?> 
+            <a href="#"> <img  class="hidden" src="img/shoppingIcon.png"></a>
+        <?php endif; ?>
     </div>
-    <!--<a href="#"> <img src="img/shoppingIcon.png"></a>-->    
 </nav>
 
