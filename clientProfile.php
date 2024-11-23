f0268b3cba1ebbecdce376021a14decdb835b0f8
Audrey <r0982547@student.thomasmore.be>
2024-11-23 21:51:59 +0100

    Feat: Deleted a comment after I fixed the bug I described in it

--- clientProfile.php
+++ clientProfile.php
@@ -38,12 +38,9 @@
     <?php include_once("nav.inc.php"); ?>
 
     <div class="user-details">
-        <h1>Welkom, <?php echo htmlspecialchars($client->getUsername());?></h1>
+        <h1>Welkom <?php echo htmlspecialchars($client->getUsername());?></h1>
         <p><strong>Email:</strong><?php echo htmlspecialchars($client->getEmail()); ?></p>
     </div>
 
 </body>
 </html>
-
-<!--FIX DEZE PAGINA: 
-    - Gebruiker logt in en geraakt op de index.php, maar als deze op het profielicoontje klikt word die 1) geherleid naar de login pagina OF 2) als ik een vardump doe, verschijnen de gevraagde gegevens bovenaan MAAR met de echo "Er is een probleem met het ophalen van je gegevens. Probeer opnieuw in te loggen aub" ernaast -->
